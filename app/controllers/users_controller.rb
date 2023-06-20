ad2661e892df540d6e16ce3e66158b51ded9f1e2
Nayan Bhalerao <rubydev.nayan@gmail.com>
2023-06-20 17:48:06 +0530

    adding the users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      puts("\n\n\n#{@user.save}\n\n\n")
+      flash[:notice] = "welcome to the alpha blog #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:username,:email,:password)
+  end
+
+
 end
