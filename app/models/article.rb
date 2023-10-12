fcf8690f67cfdd87cb1b2411df921be104561e52
Nayan Bhalerao <rubydev.nayan@gmail.com>
2023-10-12 21:33:29 +0530

    entire setup of many to many associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,7 @@ class Article < ApplicationRecord
     belongs_to :user
     validates :title, :presence => true, length: {minimum:5, maximum:25}
     validates :description, :presence => true,length: {minimum:3, maximum:300}
+    has_many :article_categories
+    has_many :categories, through: :article_categories
 end
 
