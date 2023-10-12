404b87cb234c3ee670448958d2e0231843db5d3c
Nayan Bhalerao <rubydev.nayan@gmail.com>
2023-10-12 23:05:02 +0530

    display of badges of categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,7 @@ class ArticlesController < ApplicationController
 
   # POST /articles or /articles.json
   def create
+    byebug
     @article = Article.new(article_params)
     # for assigning the user_id o article 
     # @article.user = User.first
@@ -73,7 +74,7 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description, category_ids: [])
     end
 
     # as the article cannot be editat by different users
